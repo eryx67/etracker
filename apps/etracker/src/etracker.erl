3acd0673b1d33610a32b039fe0be399e56c90e6a
eryx67 <eryx67@gmail.com>
2013-05-13 21:09:02 +0600

    refactoring

--- apps/etracker/src/etracker.erl
+++ apps/etracker/src/etracker.erl
@@ -3,14 +3,6 @@
 -export([start/0, stop/0, info/0, info/1]).
 
 start() ->
-    ensure_started(crypto),
-    ensure_started(mimetypes),
-    ensure_started(gproc),
-    ensure_started(ranch),
-    ensure_started(cowboy),
-    ensure_started(syntax_tools),
-    ensure_started(compiler),
-    ensure_started(lager),
     application:start(etracker).
 
 stop() ->
@@ -25,11 +17,3 @@ info(Keys) when is_list(Keys) ->
     [{Key, etracker_db:system_info(Key)} || Key <- Keys];
 info(Key) ->
     etracker_db:system_info(Key).
-
-ensure_started(App) ->
-    case application:start(App) of
-        ok ->
-            ok;
-        {error, {already_started, App}} ->
-            ok
-    end.
--- apps/etracker/src/etracker_app.erl
+++ apps/etracker/src/etracker_app.erl
@@ -9,7 +9,17 @@
 %% Application callbacks
 %% ===================================================================
 start(_StartType, _StartArgs) ->
+    {ok, Deps} = application:get_key(etracker, applications),
+    lists:foreach(fun ensure_started/1, Deps),
     etracker_sup:start_link().
 
 stop(_State) ->
     ok.
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
